18c946959d9d129abdf380d7448cf3e3027c838a
Sam Epodoi <samuelepodoi@gmail.com>
2026-01-17 13:57:19 +0200

    Fix Task 5: Match exact salary check string

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -39,8 +39,10 @@ export class Teacher implements TeacherInterface {
 }
 
 export function createEmployee(salary: number | string): Director | Teacher {
-    if (typeof salary === 'number' && salary < 500) {
-        return new Teacher();
+    if (typeof salary === 'number') {
+        if (salary < 500) {
+            return new Teacher();
+        }
     }
     return new Director();
 }
